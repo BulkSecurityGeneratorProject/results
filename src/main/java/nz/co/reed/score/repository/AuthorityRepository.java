f45840d3966055ddbb420504c89bff46e75f8e13
Andrew Reed <andrew.reed@skytv.co.nz>
2018-10-24 10:40:30 +1300

    Initial application generated by JHipster-5.5.0

---
+++ src/main/java/nz/co/reed/score/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package nz.co.reed.score.repository;
+
+import nz.co.reed.score.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the Authority entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
---
+++ src/main/java/nz/co/reed/score/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package nz.co.reed.score.web.rest.vm;
